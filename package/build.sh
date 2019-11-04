580ad587a1981ffdb3491640151f3f5474794f81
Oleksii Samorukov <oleksii_samorukov@mckinsey.com>
2019-11-04 11:39:57 +0100

    Remove volicon as it will make package looks like gimp itself

--- package/build.sh
+++ package/build.sh
@@ -130,7 +130,6 @@ cd create-dmg
 
 ./create-dmg \
 --volname "GIMP 2.10 Install" \
---volicon "../gimp.icns" \
 --background "../gimp-dmg.png" \
 --window-pos 1 1 \
 --icon "GIMP-2.10.app" 190 360 \
