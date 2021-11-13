4dc2107c440ea0d7865dd09ed7a2943610f8eb7e
Lukas Oberhuber <lukaso@gmail.com>
2021-11-13 10:27:53 +0000

    Get 2.99 Build working
    modulesets: update python to 3.9.7
    modulesets: cairo build for Big Sur
    
    Handle Big Sur PThreads limitations
    https://developer.apple.com/forums/thread/666700
    
    gtk-osx: Upgrade lcms to 2.12
    modulesets: lcms2 disable dependency tracking
    
    Tracks homebrew formula
    
    gtk-osx: Update to current state of GNOME/gtk-osx
    
    Incorporating changes from upstream gtk-osx
    
    For any file that is identical to a remote file, I've use the
    remote/upstream moduleset. For remaining modulesets, I've used local
    copies.
    I've also brought as many patches as possibly, local.
    
    Includes new patch for:
    https://gitlab.freedesktop.org/freetype/freetype/-/issues/1117
    
    Further steps would include getting `gtk-osx-setup.sh` and
    `jhbuildrc-gtk-osx` directly from upstream.
    Final step would be to get most modules from upstream with only
    `gtk-osx-gimp.modules` coming from this repo.
    
    gtk-osx-setup: Ensure unattended install
    gdk-pixbuf: Must not be relocatable
    
    Otherwise pixbuf loading will not work due to a hard coded relocation
    path that cannot be changed.

--- gtk-osx-setup.sh
+++ gtk-osx-setup.sh
@@ -59,7 +59,6 @@ envvar PYENV_ROOT "$DEVPREFIX/share/pyenv"
 envvar PIP_CONFIG_DIR "$HOME/.config/pip"
 
 export PYTHONWARNINGS=ignore:DEPRECATION::pip._internal.cli.base_command
-export PYTHONPATH="$PYTHONUSERBASE/lib/python/site-packages":"$PYTHONPATH"
 
 if test ! -d "$DEVPREFIX/bin" ; then
     mkdir -p "$DEVPREFIX/bin"
@@ -78,102 +77,125 @@ if test ! -x "$PYENV_INSTALL_ROOT/libexec/pyenv"; then
   if test -d "$PYENV_INSTALL_ROOT"; then
      rm -rf "$PYENV_INSTALL_ROOT";
   fi
-    git clone $GITHUB/pyenv/pyenv.git "$PYENV_INSTALL_ROOT"
+  git clone $GITHUB/pyenv/pyenv.git "$PYENV_INSTALL_ROOT"
+else
+    pushd "$PYENV_INSTALL_ROOT"
+    git pull --ff-only
+    popd
 fi
 
-if test ! -x "$DEVPREFIX/bin/pyenv" ; then
+PYENV="$DEVPREFIX/bin/pyenv"
+
+if test ! -x "$PYENV" ; then
     ln -s "$PYENV_INSTALL_ROOT/bin/pyenv" "$DEVPREFIX/bin"
 fi
 
-# Setup PIP; note that we're assuming that python is the system python
-# at this point. Having set $PYTHONUSERBASE, pip will be installed in
-# $PYTHONUSERBASE/bin and the requisite modules will go in
-# $PYTHONUSERBASE/lib/python/site-packages.
+#Force installation and we hope use of Python with PyEnv. We must
+#avoid using the Apple-provided Python2 because jhbuild doesn't work
+#with python2 any more, and the Apple-provided python3 because it
+#doesn't include a usable libpython for libxml2 to link against.
 
-if test ! -f "`eval echo $PIP_CONFIG_FILE`" ; then
-    export PIP_CONFIG_FILE="$PIP_CONFIG_DIR/pip.conf"
-    mkdir -p "$PIP_CONFIG_DIR"
-fi
-PIP=`which pip`
-if test ! -x "`eval echo $PIP`" ; then
-    mv=`python --version 2>&1 | cut -b 12-13`
-    if test $mv -lt 11 ; then
-        curl https://bootstrap.pypa.io/pip/3.4/get-pip.py -o "$DEVPREFIX/get-pip.py"
-        python "$DEVPREFIX/get-pip.py" --user
-        rm "$DEVPREFIX/get-pip.py"
-    else
-        python -m ensurepip --user
-    fi
-    PIP="$PYTHONUSERBASE/bin/pip"
-    $PIP install --upgrade --user pip
-fi
+export PYTHON_CONFIGURE_OPTS="--enable-shared"
+export PYENV_VERSION=3.10.0
+$PYENV install $PYENV_VERSION --skip-existing
+PIP="$PYENV_ROOT/shims/pip"
+$PIP install --upgrade --user pip
 
 # Install pipenv
-# Locking pipenv until https://github.com/pypa/pipenv/issues/4834 is resolved
-$PIP install --upgrade --user pipenv==2021.5.29
-pip_remove typing
+$PIP install --upgrade --user pipenv==2020.11.15
 PIPENV="$PYTHONUSERBASE/bin/pipenv"
+export WORKON_HOME=$DEVPREFIX/share/virtualenvs
 
+JHBUILD_RELEASE_VERSION=3.38.0
 # Install jhbuild
 if test ! -d "$DEV_SRC_ROOT/jhbuild/.git" ; then
-    git clone -b "3.38.0" $GITLAB/jhbuild.git "$DEV_SRC_ROOT/jhbuild"
+    git clone -b $JHBUILD_RELEASE_VERSION $GITLAB/jhbuild.git "$DEV_SRC_ROOT/jhbuild"
     cd "$DEV_SRC_ROOT/jhbuild"
 else #Get the latest if it's already installed
     cd "$DEV_SRC_ROOT/jhbuild"
-    git co "3.38.0"
-    git pull origin "3.38.0"
+    git pull
+    git reset --hard $JHBUILD_RELEASE_VERSION
 fi
 
 # Install Ninja
 NINJA=`which ninja`
 if test ! -x "$NINJA" -a ! -x "$DEVPREFIX/bin/ninja"; then
-    curl -kLs https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-mac.zip -o "$DEVPREFIX/ninja-mac.zip"
+    curl -kLs https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-mac.zip -o "$DEVPREFIX/ninja-mac.zip"
     unzip -d "$DEVPREFIX/bin" "$DEVPREFIX/ninja-mac.zip"
     rm "$DEVPREFIX/ninja-mac.zip"
+fi
+
+#Install Rust (required for librsvg, which gtk needs to render its icons.)
+RUSTUP=`which rustup`
+if test -x "$RUSTUP"; then
+    case `dirname "$RUSTUP"` in
+        "$DEVPREFIX"*)
+            DEFAULT_CARGO_HOME=$(dirname $(dirname "$RUSTUP"))
+            envvar CARGO_HOME "$DEFAULT_CARGO_HOME"
+            CARGO_HOME_DIR=$(basename "$CARGO_HOME")
+            if test "$CARGO_HOME_DIR" == "cargo"; then
+                CARGO_HOME_BASE=$(dirname "$CARGO_HOME")
+                envvar RUSTUP_HOME "$CARGO_HOME_BASE"/rustup
+            else
+                envvar RUSTUP_HOME "$CARGO_HOME"
+            fi
+            ;;
+        *)
+            envvar CARGO_HOME "$HOME/.cargo"
+            envvar RUSTUP_HOME "$HOME/.rustup"
+            ;;
+    esac
+else
+    envvar CARGO_HOME "$DEVPREFIX"
+    envvar RUSTUP_HOME "$DEVPREFIX"
+    curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --no-modify-path 
 fi
 
 if test ! -d "$DEVPREFIX/etc" ; then
     mkdir -p "$DEVPREFIX/etc"
 fi
-if test ! -f "$DEVPREFIX/etc/Pipfile" ; then
-    cat  <<EOF > "$DEVPREFIX/etc/Pipfile"
+
+PYENV_MINOR_VERSION=$(echo $PYENV_VERSION | cut -d . -f 1,2)
+cat  <<EOF > "$DEVPREFIX/etc/Pipfile"
 [[source]]
 url = "https://pypi.python.org/simple"
 verify_ssl = true
 
 [packages]
-six = "*"
-meson = {version="==0.59.1"}
+pygments = "*"
+meson = {version=">=0.59.1"}
 
 [scripts]
 jhbuild = "$DEVPREFIX/libexec/run_jhbuild.py"
 
 [requires]
-python_version = "3.9"
+python_version = "$PYENV_MINOR_VERSION"
 EOF
-    cat <<EOF > "$DEVPREFIX/etc/pipenv-env"
+cat <<EOF > "$DEVPREFIX/etc/pipenv-env"
 export PYTHONUSERBASE="$PYTHONUSERBASE"
 export DEV_SRC_ROOT="$DEV_SRC_ROOT"
 export PYENV_ROOT="$PYENV_ROOT"
 export PIP_CONFIG_DIR="$PIP_CONFIG_DIR"
 export LANG=C
 EOF
-fi
-if test ! -f "$DEVPREFIX/bin/jhbuild" ; then
-    cat <<EOF > "$DEVPREFIX/bin/jhbuild"
+
+cat <<EOF > "$DEVPREFIX/bin/jhbuild"
 #!$DEVPREFIX/bin/bash
 export DEVROOT="$DEVROOT"
 export DEVPREFIX="$DEVPREFIX"
-export WORKON_HOME="$DEVPREFIX/share/venv"
-export PYTHONPATH="$PYTHONPATH"
+export PYTHONUSERBASE="$PYTHONUSERBASE"
 export PIPENV_DOTENV_LOCATION="$DEVPREFIX/etc/pipenv-env"
 export PIPENV_PIPFILE="$DEVPREFIX/etc/Pipfile"
-export PATH="$PYENV_ROOT/shims:$DEVPREFIX/bin:$PATH"
 export PYENV_ROOT="$PYENV_ROOT"
+export PYENV_VERSION="$PYENV_VERSION"
+export PATH="$PYENV_ROOT/shims:$PATH"
+export CARGO_HOME="$CARGO_HOME"
+export RUSTUP_HOME="$RUSTUP_HOME"
+export WORKON_HOME="$WORKON_HOME"
 
-exec pipenv run jhbuild \$@
+exec $DEVPREFIX/bin/pipenv run jhbuild \$@
 EOF
-fi
+
 if test ! -d "$DEVPREFIX/libexec" ; then
     mkdir -p "$DEVPREFIX/libexec"
 fi
@@ -200,6 +222,7 @@ import jhbuild.main
 jhbuild.main.main(sys.argv[1:])
 
 EOF
+
 if test ! -x "$DEVPREFIX/bin/jhbuild" ; then
     chmod +x "$DEVPREFIX/bin/jhbuild"
 fi
@@ -212,19 +235,19 @@ if test "x`echo $PATH | grep "$DEVPREFIX/bin"`" == x ; then
 fi
 # pipenv wants enum34 because it's installed with Py2 but that conflicts
 # with Py3 so remove it.
-pip_remove enum34
+if test $PYVER -eq 3; then
+    pip_remove enum34
+fi
 
 SDKROOT=`xcrun --show-sdk-path`
 
 export PIPENV_DOTENV_LOCATION="$DEVPREFIX/etc/pipenv-env"
 export PIPENV_PIPFILE="$DEVPREFIX/etc/Pipfile"
 export PATH="$PYENV_ROOT/shims:$DEVPREFIX/bin:$PYENV_INSTALL_ROOT/plugins/python-build/bin:$PATH"
-export PYENV_ROOT
+
 if test -d "$SDKROOT"; then
     export CFLAGS="-isysroot $SDKROOT -I$SDKROOT/usr/include"
 fi
-export PYTHON_CONFIGURE_OPTS="--enable-shared"
-export WORKON_HOME="$DEVPREFIX/share/venv"
 
 $PIPENV install
 
