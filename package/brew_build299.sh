b8d76b78a83d94e08b8b7fa127da747ea801c785
Lukas Oberhuber <lukaso@gmail.com>
2022-07-25 00:55:47 +0100

    package brew: robust vers detect, copy extensions
    - Version detection more robust - (doesn't fail if stderr has content
    - Extensions are now copied into release package

--- package/brew_build299.sh
+++ package/brew_build299.sh
@@ -22,7 +22,7 @@ printf "Determining GIMP version: "
 
 $(cd ${PREFIX}/bin/ && ln -sf gimp-2.99 gimp)
 
-GIMP_VERSION="$(gimp-2.99 --version | sed 's|GNU Image Manipulation Program version ||')"
+GIMP_VERSION="$(~/gtk/inst/bin/gimp-2.99 --version 2>/dev/null | grep 'GNU Image Manipulation Program version' | sed 's|GNU Image Manipulation Program version ||')"
 # for gtk-mac-bundler
 export GIMP_CELLAR="${PREFIX}/Cellar/gimp3/${GIMP_VERSION}"
 
