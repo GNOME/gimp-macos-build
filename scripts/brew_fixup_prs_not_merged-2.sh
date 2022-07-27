7f41c171fa33ef95ad73abe2f27ef8fcf4a2b4a0
Lukas Oberhuber <lukaso@gmail.com>
2022-07-27 08:43:48 +0100

    Add libunistring open PR to fixup PRs

--- scripts/brew_fixup_prs_not_merged-2.sh
+++ scripts/brew_fixup_prs_not_merged-2.sh
@@ -12,5 +12,9 @@ pushd ${PREFIX}/Library/Taps/homebrew/homebrew-core
 git remote add lukaso git@github.com:lukaso/homebrew-core.git
 git fetch lukaso
 # https://github.com/Homebrew/homebrew-core/pull/105614
+# ghostscript
 git cherry-pick bdfcd5ec34a74ca20e0d30e109164d7d585ee951
+# https://github.com/Homebrew/homebrew-core/pull/106654
+# libunistring
+git cherry-pick 60360d41ecf75e7afe9164babcf1914a17d9d3d6
 popd
