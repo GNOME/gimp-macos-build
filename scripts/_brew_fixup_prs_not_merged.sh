1b34597deaf6558eb3465254cd7683a73e6f0346
Lukas Oberhuber <lukaso@gmail.com>
2022-08-16 21:42:01 -0700

    homebrew: more prs are merged

--- scripts/_brew_fixup_prs_not_merged.sh
+++ scripts/_brew_fixup_prs_not_merged.sh
@@ -11,16 +11,10 @@ fi
 pushd ${PREFIX}/Library/Taps/homebrew/homebrew-core
 git remote add lukaso git@github.com:lukaso/homebrew-core.git
 git fetch lukaso
-# https://github.com/Homebrew/homebrew-core/pull/105614
-# ghostscript
-git cherry-pick bdfcd5ec34a74ca20e0d30e109164d7d585ee951
-# https://github.com/Homebrew/homebrew-core/pull/106654
 # libunistring
-git cherry-pick 60360d41ecf75e7afe9164babcf1914a17d9d3d6
-# https://github.com/Homebrew/homebrew-core/pull/106658
-# gsettings-desktop-schemas
-git cherry-pick 5f6eda4a0fc468e92c49f20c0b48fb85a37ad9b6
+# https://github.com/Homebrew/homebrew-core/pull/106654
+git cherry-pick 7941db447dc903c621be20a0e8aaeae6252d9089
 # poppler
 # https://github.com/Homebrew/homebrew-core/pull/106665
-git cherry-pick 0129523ec1026a9f8ba0a22c393db03055360bef
+git cherry-pick a0d2522d0c174f1ddfb1485909f957fac4824169
 popd
