4e1c0687c9a93556abed52bcab66e4ac66ebc28f
Lukas Oberhuber <lukaso@gmail.com>
2022-08-29 00:52:30 +0100

    homebrew: update libunistring and poppler

--- scripts/_brew_fixup_prs_not_merged.sh
+++ scripts/_brew_fixup_prs_not_merged.sh
@@ -12,9 +12,9 @@ pushd ${PREFIX}/Library/Taps/homebrew/homebrew-core
 git remote add lukaso git@github.com:lukaso/homebrew-core.git
 git fetch lukaso
 # libunistring
-# https://github.com/Homebrew/homebrew-core/pull/106654
-git cherry-pick 7941db447dc903c621be20a0e8aaeae6252d9089
+# https://github.com/Homebrew/homebrew-core/pull/109128
+git cherry-pick 9fee4fb5b5ece28da26a5e92027573c797100441
 # poppler
 # https://github.com/Homebrew/homebrew-core/pull/106665
-git cherry-pick a0d2522d0c174f1ddfb1485909f957fac4824169
+git cherry-pick 7dfe8ca1cd8410b9019a1469614c2fb58c911f76
 popd
