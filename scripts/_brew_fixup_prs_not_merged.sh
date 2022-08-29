b3a58930cc167e4b221eb8a71ba17163d8c09ec8
Lukas Oberhuber <lukaso@gmail.com>
2022-08-29 23:36:24 +0100

    homebrew: add prs

--- scripts/_brew_fixup_prs_not_merged.sh
+++ scripts/_brew_fixup_prs_not_merged.sh
@@ -13,7 +13,7 @@ git remote add lukaso git@github.com:lukaso/homebrew-core.git
 git fetch lukaso
 # libunistring
 # https://github.com/Homebrew/homebrew-core/pull/109128
-git cherry-pick 9fee4fb5b5ece28da26a5e92027573c797100441
+git cherry-pick f55ca34f92b96b9aa3887b9a84c63e870d1d034f
 # poppler
 # https://github.com/Homebrew/homebrew-core/pull/106665
 git cherry-pick 7dfe8ca1cd8410b9019a1469614c2fb58c911f76
