bae538bdaea90e3d9c4798315ac6f4beef2fa032
Lukas Oberhuber <lukaso@gmail.com>
2022-09-03 15:54:20 +0100

    homebrew PRs: remove popplar

--- scripts/_brew_fixup_prs_not_merged.sh
+++ scripts/_brew_fixup_prs_not_merged.sh
@@ -14,7 +14,4 @@ git fetch lukaso
 # libunistring
 # https://github.com/Homebrew/homebrew-core/pull/109128
 git cherry-pick f55ca34f92b96b9aa3887b9a84c63e870d1d034f
-# poppler
-# https://github.com/Homebrew/homebrew-core/pull/106665
-git cherry-pick 7dfe8ca1cd8410b9019a1469614c2fb58c911f76
 popd
