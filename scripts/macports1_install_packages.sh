d596e347243e56ee1a9470e59613ae90982423fb
Lukas Oberhuber <lukaso@gmail.com>
2023-02-21 23:31:50 +0000

    macports: get pre-built cmake while building deps
    This pre builds all cmake dependencies while getting pre-build cmake

--- scripts/macports1_install_packages.sh
+++ scripts/macports1_install_packages.sh
@@ -173,9 +173,18 @@ if [ -n "${UNINSTALL_PACKAGE}" ]; then
 fi
 
 if [ -n "${PART1}" ]; then
+  echo "build cmake dependencies in case they are needed for gimp"
+  port_clean_and_install  libcxx \
+                          curl \
+                          expat \
+                          zlib \
+                          bzip2 \
+                          libarchive \
+                          ncurses \
+                          libuv
   echo "cmake-bootstrap being installed since won't build from source with 10.12 SDK"
   $dosudo sed -i -e 's/buildfromsource always/buildfromsource ifneeded/g' /opt/local/etc/macports/macports.conf
-  port_clean_and_install cmake-bootstrap
+  port_clean_and_install cmake
   $dosudo sed -i -e 's/buildfromsource ifneeded/buildfromsource always/g' /opt/local/etc/macports/macports.conf
 
   port_clean_and_install p5.34-io-compress-brotli build.jobs=1
