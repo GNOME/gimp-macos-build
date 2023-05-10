021c623ff2df39d7673e30e619f0b6d531a41146
Lukas Oberhuber <lukaso@gmail.com>
2023-05-10 09:19:32 +0100

    circleci: altool to notarytool
    (cherry picked from commit c5941774d3bbc348e70fe7d843960a4d78696112)
    
    test different order for log command
    
    (cherry picked from commit eaf713347d314c09e1b7c50adb2203f926d0b3e1)

--- package/notarize.sh
+++ package/notarize.sh
@@ -1,61 +1,34 @@
 #!/bin/bash
 
-HEX="[0-9a-f"]
-UUID_REGEX="($HEX{8}-$HEX{4}-$HEX{4}-$HEX{4}-$HEX{12})"
 DMG_FILE=$(find /tmp/artifacts/ -name "gimp-2.10*.dmg")
 
-# Submit for notarization and get our ticket ID if submission is successful
-ALTOOL_OUT="$(xcrun altool --notarize-app --file ${DMG_FILE} -u ${notarization_login} --primary-bundle-id org.gimp.gimp-2.10 -p ${notarization_password} 2>&1)" || ALTOOL_FAILED=true
+# Submit for notarization
+NOTARY_OUT="$(xcrun notarytool submit ${DMG_FILE} --apple-id ${notarization_login} --team-id ${notarization_teamid} --password ${notarization_password} --wait 2>&1)"
 
-echo "$ALTOOL_OUT"
+echo "$NOTARY_OUT"
 
-if [[ $ALTOOL_FAILED ]]; then
-  # In case the error is "The software asset has already been uploaded",
-  # we can still find the UUID prefix and continue.
-  UUID_PREFIX="The upload ID is "
-else
-  UUID_PREFIX="RequestUUID[[:space:]]*=[[:space:]]*"
-fi
+# Extract Request UUID
+REQUEST_UUID=$(echo "$NOTARY_OUT" | grep -oE "id: [0-9a-f-]+" | awk '{print $2}')
 
-if [[ $ALTOOL_OUT =~ $UUID_PREFIX($UUID_REGEX) ]]; then
-  REQUEST_UUID=${BASH_REMATCH[1]}
-else
-  echo "Failed finding Request UUID in altool output"
+if [ -z "$REQUEST_UUID" ]; then
+  echo "Failed finding Request UUID in notarytool output"
   exit 1
 fi
 
-echo "$REQUEST_UUID"
+echo "Request UUID: $REQUEST_UUID"
 
-# Now we loop around, waiting for the notarization request to complete so we can check its status
-for run in {1..360}; do
-  sleep 60
-  NOT_INFO="$(xcrun altool --notarization-info $REQUEST_UUID -u ${notarization_login} -p ${notarization_password}  2>&1)" || echo "$NOT_INFO"
-  NOT_STATUS=$(echo "$NOT_INFO" | grep Status: | awk -F ": " '{print $NF}')
-  NOT_LOGURL=$(echo "$NOT_INFO" | grep LogFileURL: | awk -F ": " '{print $NF}')
-  if [[ "$NOT_STATUS" == "in progress" ]]; then
-     echo "Notarization in progress. Waiting 60 seconds before trying again."
-     continue
-  elif [[ "$NOT_STATUS" == "invalid" ]]; then
-     echo "Notarization failed with status invalid. Showing log"
-     curl "$NOT_LOGURL"
-     exit 1
-  elif [[ "$NOT_STATUS" == "success" ]]; then
-     echo "Notarization succeeded. Showing log"
-     curl "$NOT_LOGURL"
-     break
-  elif [[ "$NOT_INFO" == *"Could not find the RequestUUID"* ]]; then
-     echo "Waiting 60 seconds before trying again..."
-     continue
-  else
-     echo "Error: Unknown notarization status $NOT_STATUS."
-     exit 1
-  fi
-done
+NOT_STATUS=$(echo "$NOTARY_OUT" | grep status: | awk -F ": " '{print $NF}')
 
-if [[ "$NOT_STATUS" == "in progress" ]]; then
-  echo "Error: Notarization timed out."
+if [[ "$NOT_STATUS" == Accepted* ]]; then
+  echo "Notarization succeeded. Showing log"
+else
+  echo "Notarization failed with status: $NOT_STATUS. Showing log"
+fi
+
+xcrun notarytool log $REQUEST_UUID --apple-id ${notarization_login} --team-id ${notarization_teamid} --password ${notarization_password}
+
+if [[ "$NOT_STATUS" != Accepted* ]]; then
   exit 1
 fi
 
 xcrun stapler staple -v ${DMG_FILE}
-
