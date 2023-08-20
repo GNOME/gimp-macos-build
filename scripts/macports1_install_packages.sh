ece27ce84cc71bd1bc50fe320ea807a2568c772f
Lukas Oberhuber <lukaso@gmail.com>
2023-08-20 01:24:37 +0100

    macports: simplify making release
    (cherry picked from commit f71610f15cfd23ca3e4ba9d3ff3b129f886cdefb)

--- scripts/macports1_install_packages.sh
+++ scripts/macports1_install_packages.sh
@@ -1,68 +1,70 @@
 #!/usr/bin/env bash
 #####################################################################
- # macports1_install_packages.sh: installs gimp dependencies         #
- #                                                                  #
- # Copyright 2022 Lukas Oberhuber <lukaso@gmail.com>                #
- #                                                                  #
- # This program is free software; you can redistribute it and/or    #
- # modify it under the terms of the GNU General Public License as   #
- # published by the Free Software Foundation; either version 2 of   #
- # the License, or (at your option) any later version.              #
- #                                                                  #
- # This program is distributed in the hope that it will be useful,  #
- # but WITHOUT ANY WARRANTY; without even the implied warranty of   #
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    #
- # GNU General Public License for more details.                     #
- #                                                                  #
- # You should have received a copy of the GNU General Public License#
- # along with this program; if not, contact:                        #
- #                                                                  #
- # Free Software Foundation           Voice:  +1-617-542-5942       #
- # 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652       #
- # Boston, MA  02110-1301,  USA       gnu@gnu.org                   #
- ####################################################################
+# macports1_install_packages.sh: installs gimp dependencies         #
+#                                                                  #
+# Copyright 2022 Lukas Oberhuber <lukaso@gmail.com>                #
+#                                                                  #
+# This program is free software; you can redistribute it and/or    #
+# modify it under the terms of the GNU General Public License as   #
+# published by the Free Software Foundation; either version 2 of   #
+# the License, or (at your option) any later version.              #
+#                                                                  #
+# This program is distributed in the hope that it will be useful,  #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of   #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    #
+# GNU General Public License for more details.                     #
+#                                                                  #
+# You should have received a copy of the GNU General Public License#
+# along with this program; if not, contact:                        #
+#                                                                  #
+# Free Software Foundation           Voice:  +1-617-542-5942       #
+# 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652       #
+# Boston, MA  02110-1301,  USA       gnu@gnu.org                   #
+####################################################################
 
-set -e;
+set -e
 
 export VGIMP=2
 
 function pure_version() {
-	echo '0.1'
+  echo '0.1'
 }
 
 function version() {
-	echo "macports1_install_packages.sh $(pure_version)"
+  echo "macports1_install_packages.sh $(pure_version)"
 }
 
 function usage() {
-    version
-    echo ""
-    echo "Builds Gimp 3 dependencies."
-    echo "Usage:  $(basename $0) [options]"
-    echo ""
-    echo "Builds Gimp dependencies."
-    echo "By default builds dependencies, end to end."
-    echo "For CI builds, can be split into steps to reduce run time for each"
-    echo "step."
-    echo "Options:"
-    echo "  --part1"
-    echo "      first part. Each part takes up to 3 hours on circleci"
-    echo "  --part2"
-    echo "      second part."
-    echo "  --part3"
-    echo "      third part."
-    echo "  --part4"
-    echo "      fourth part"
-    echo "  --part5"
-    echo "      currently a no op"
-    echo "  --only-package <package>"
-    echo "      only install named package. Most useful for testing"
-    echo "  --uninstall-package <package>"
-    echo "      uninstall named package. Useful if needed to clear things out."
-    echo "      This will force uninstall regardless of dependencies."
-    echo "  --version         show tool version number"
-    echo "  -h, --help        display this help"
-    exit 0
+  version
+  echo ""
+  echo "Builds Gimp 2 dependencies."
+  echo "Usage:  $(basename $0) [options]"
+  echo ""
+  echo "Builds Gimp dependencies."
+  echo "By default builds dependencies, end to end."
+  echo "For CI builds, can be split into steps to reduce run time for each"
+  echo "step."
+  echo "Options:"
+  echo "  --part1"
+  echo "      first part. Each part takes up to 3 hours on circleci"
+  echo "  --part2"
+  echo "      second part"
+  echo "  --part3"
+  echo "      third part"
+  echo "  --part4"
+  echo "      fourth part"
+  echo "  --part5"
+  echo "      currently a no op"
+  echo "  --only-package <package>"
+  echo "      only install named package. Most useful for testing"
+  echo "  --uninstall-package <package>"
+  echo "      uninstall named package. Useful if needed to clear things out."
+  echo "      This will force uninstall regardless of dependencies"
+  echo "  --port-edit-package <package>"
+  echo "      open the Portfile of the package"
+  echo "  --version         show tool version number"
+  echo "  -h, --help        display this help"
+  exit 0
 }
 
 PART1="true"
@@ -72,60 +74,75 @@ PART4="true"
 PART5="true"
 
 while test "${1:0:1}" = "-"; do
-	case $1 in
-	--part1)
-		PART2=''
-		PART3=''
-		PART4=''
-		PART5=''
-		shift;;
-	--part2)
-		PART1=''
-		PART3=''
-		PART4=''
-		PART5=''
-		shift;;
-	--part3)
-		PART1=''
-		PART2=''
-		PART4=''
-		PART5=''
-		shift;;
-	--part4)
-		PART1=''
-		PART2=''
-		PART3=''
-		PART5=''
-		shift;;
-	--part5)
-		PART1=''
-		PART2=''
-		PART3=''
-		PART4=''
-		shift;;
+  case $1 in
+  --part1)
+    PART2=''
+    PART3=''
+    PART4=''
+    PART5=''
+    shift
+    ;;
+  --part2)
+    PART1=''
+    PART3=''
+    PART4=''
+    PART5=''
+    shift
+    ;;
+  --part3)
+    PART1=''
+    PART2=''
+    PART4=''
+    PART5=''
+    shift
+    ;;
+  --part4)
+    PART1=''
+    PART2=''
+    PART3=''
+    PART5=''
+    shift
+    ;;
+  --part5)
+    PART1=''
+    PART2=''
+    PART3=''
+    PART4=''
+    shift
+    ;;
   --only-package)
     ONLY_PACKAGE=$2
-    shift 2;;
+    shift 2
+    ;;
   --uninstall-package)
     UNINSTALL_PACKAGE=$2
-    shift 2;;
-	-h | --help)
-		usage;;
-	--version)
-		version; exit 0;;
-	-*)
-		echo "Unknown option $1. Run with --help for help."
-		exit 1;;
-	esac
+    shift 2
+    ;;
+  --port-edit-package)
+    PORT_EDIT_PACKAGE=$2
+    shift 2
+    ;;
+  -h | --help)
+    usage
+    ;;
+  --version)
+    version
+    exit 0
+    ;;
+  -*)
+    echo "Unknown option $1. Run with --help for help."
+    exit 1
+    ;;
+  esac
 done
 
-PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 source ~/.profile-gimp${VGIMP}
 export PATH=$PREFIX/bin:$PATH
 
 function massage_output() {
-	if [ -n "$circleci" ]; then
+  if [ -n "$circleci" ]; then
     # suppress progress bar
     "$@" | cat
     status="${PIPESTATUS[0]}"
@@ -174,6 +191,11 @@ if [ -n "${UNINSTALL_PACKAGE}" ]; then
   exit 0
 fi
 
+if [ -n "${PORT_EDIT_PACKAGE}" ]; then
+  port edit "${PORT_EDIT_PACKAGE}"
+  exit 0
+fi
+
 if [ -n "${PART1}" ]; then
   # ** Reinstate these uninstalls if builds fail
   # temporarily uninstall gegl, gimp3, libgcc12 (until all builds are fixed)
@@ -191,19 +213,19 @@ if [ -n "${PART1}" ]; then
   # $dosudo port uninstall clang-15 || true
   # $dosudo port uninstall llvm-15 || true
   echo "build cmake dependencies in case they are needed for gimp"
-  port_clean_and_install  libcxx \
-                          curl \
-                          expat \
-                          zlib \
-                          bzip2 \
-                          libarchive \
-                          ncurses \
-                          libuv
+  port_clean_and_install libcxx \
+    curl \
+    expat \
+    zlib \
+    bzip2 \
+    libarchive \
+    ncurses \
+    libuv
   echo "cmake-bootstrap being installed since won't build from source with 10.13 SDK"
   $dosudo sed -i -e 's/buildfromsource always/buildfromsource ifneeded/g' ${PREFIX}/etc/macports/macports.conf
   $dosudo sed -i -e 's/macosx_deployment_target/#macosx_deployment_target/g' ${PREFIX}/etc/macports/macports.conf
   $dosudo sed -i -e 's/macosx_sdk_version/#macosx_sdk_version/g' ${PREFIX}/etc/macports/macports.conf
-(
+  (
     # temporarily unset deployment targets since cmake does not build with 10.13 SDK
     # and we don't really care since only used at build time
     # Can be removed once https://trac.macports.org/ticket/66953 is fixed
@@ -231,14 +253,14 @@ if [ -n "${PART1}" ]; then
   (
     unset MACOSX_DEPLOYMENT_TARGET
     unset SDKROOT
-    $dosudo port clean         rust
+    $dosudo port clean rust
     # Must be verbose because otherwise times out on circle ci
     $dosudo port -v -N install rust
   )
   $dosudo sed -i -e 's/buildfromsource ifneeded/buildfromsource always/g' ${PREFIX}/etc/macports/macports.conf
   $dosudo sed -i -e 's/#macosx_deployment_target/macosx_deployment_target/g' ${PREFIX}/etc/macports/macports.conf
   $dosudo sed -i -e 's/#macosx_sdk_version/macosx_sdk_version/g' ${PREFIX}/etc/macports/macports.conf
-  $dosudo port clean         llvm-15
+  $dosudo port clean llvm-15
   # Must be verbose because otherwise times out on circle ci
   $dosudo port -v -N install llvm-15
 fi
@@ -252,49 +274,49 @@ if [ -n "${PART2}" ]; then
   # $dosudo port select --set python python310
   # $dosudo port select --set python3 python310
   port_clean_and_install \
-                aalib \
-                appstream-glib \
-                exiv2 \
-                git \
-                gexiv2 \
-                ghostscript \
-                glib-networking \
-                gnutls \
-                gtk-osx-application-gtk2 \
-                gtk2 \
-                iso-codes \
-                lcms2 \
-                libde265 \
-                libheif \
-                libjpeg-turbo \
-                libjxl \
-                libmng \
-                libmypaint \
-                libpsl \
-                libraw \
-                librsvg \
-                libtasn1 \
-                libunistring \
-                libwmf \
-                mypaint-brushes1 \
-                nasm \
-                nettle \
-                openexr \
-                openjpeg \
-                openssl \
-                poly2tri-c \
-                poppler -boost \
-                poppler-data \
-                python27 \
-                readline \
-                shared-mime-info \
-                vala \
-                webp \
-                x265
+    aalib \
+    appstream-glib \
+    exiv2 \
+    git \
+    gexiv2 \
+    ghostscript \
+    glib-networking \
+    gnutls \
+    gtk-osx-application-gtk2 \
+    gtk2 \
+    iso-codes \
+    lcms2 \
+    libde265 \
+    libheif \
+    libjpeg-turbo \
+    libjxl \
+    libmng \
+    libmypaint \
+    libpsl \
+    libraw \
+    librsvg \
+    libtasn1 \
+    libunistring \
+    libwmf \
+    mypaint-brushes1 \
+    nasm \
+    nettle \
+    openexr \
+    openjpeg \
+    openssl \
+    poly2tri-c \
+    poppler -boost \
+    poppler-data \
+    python27 \
+    readline \
+    shared-mime-info \
+    vala \
+    webp \
+    x265
 fi
 
 if [ -n "${PART3}" ]; then
-  $dosudo port clean         clang-16
+  $dosudo port clean clang-16
   $dosudo port -v -N install clang-16
 fi
 
@@ -308,7 +330,7 @@ if [ -n "${PART4}" ]; then
     unset MACOSX_DEPLOYMENT_TARGET
     unset SDKROOT
     port_clean_and_install \
-                  libomp -debug
+      libomp -debug
     # * uninstall because if not, then get a failure on building
     # * clean because if not, then get random failure
     # * uninstall -f to avoid:
@@ -318,12 +340,12 @@ if [ -n "${PART4}" ]; then
     #    Too long with no output (exceeded 30m0s): context deadline exceeded"
     # * libgcc12 because it is a dependency of gcc12 and if not also uninstalled, gcc12 build sometimes fails
     # $dosudo port uninstall -f libgcc12 || true
-    $dosudo port clean     libgcc12
+    $dosudo port clean libgcc12
     # $dosudo port uninstall -f gcc12 || true
-    $dosudo port clean     gcc12
+    $dosudo port clean gcc12
     $dosudo port -v -N install \
-                  libgcc12 \
-                  gcc12
+      libgcc12 \
+      gcc12
   )
   $dosudo sed -i -e 's/buildfromsource ifneeded/buildfromsource always/g' ${PREFIX}/etc/macports/macports.conf
   $dosudo sed -i -e 's/#macosx_deployment_target/macosx_deployment_target/g' ${PREFIX}/etc/macports/macports.conf
@@ -332,9 +354,9 @@ if [ -n "${PART4}" ]; then
   # $dosudo port clean dbus
   # port_install -f dbus
   port_clean_and_install \
-                adwaita-icon-theme \
-                babl \
-                gegl +vala
+    adwaita-icon-theme \
+    babl \
+    gegl +vala
   if [ $circleci ]; then
     $dosudo port clean git
     port_install git -perl5_34
