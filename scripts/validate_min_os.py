d6db8b102b051ba4ba26c0626bdebcf4950dc740
Lukas Oberhuber <lukas@hiphops.io>
2024-05-13 16:00:07 +0100

    macports: GIMP revision 1

--- scripts/validate_min_os.py
+++ scripts/validate_min_os.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python3
 
+####################################################################
+# validate-dylib.sh: validate earlier macos supp in execs and      #
+# .dylibs                                                          #
+#                                                                  #
+# Copyright 2024 Lukas Oberhuber <lukaso@gmail.com>                #
+#                                                                  #
+# This program is free software; you can redistribute it and/or    #
+# modify it under the terms of the GNU General Public License as   #
+# published by the Free Software Foundation; either version 2 of   #
+# the License, or (at your option) any later version.              #
+#                                                                  #
+# This program is distributed in the hope that it will be useful,  #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of   #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    #
+# GNU General Public License for more details.                     #
+#                                                                  #
+# You should have received a copy of the GNU General Public License#
+# along with this program; if not, contact:                        #
+#                                                                  #
+# Free Software Foundation           Voice:  +1-617-542-5942       #
+# 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652       #
+# Boston, MA  02110-1301,  USA       gnu@gnu.org                   #
+####################################################################
+
 import os
 import subprocess
 import sys
