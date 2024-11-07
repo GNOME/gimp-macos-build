78f9c6c26dc114feb6acac51814cafde983434d7
Lukas Oberhuber <lukas@hiphops.io>
2024-11-07 00:09:09 +0000

    package: fixed checksums
    Had to be updated for 3 naming vs. 2.99

--- package/checksum-artifacts.sh
+++ package/checksum-artifacts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Calculating checksum"
-DMGNAME=$(find /tmp/artifacts -name "gimp-2.*.dmg")
+DMGNAME=$(find /tmp/artifacts -name "gimp-3.*.dmg")
 shasum -a 256 "${DMGNAME}" > "${DMGNAME}.sha256"
 echo "Checksum: $(cat "${DMGNAME}.sha256")"
