1db8d57eb0e0eee2db3b8040d8f138e00f3c6407
Lukas Oberhuber <lukas@hiphops.io>
2026-03-20 00:23:18 +0000

    package: Fix #79 orphaned pkg-config files

--- package/macports_build_plugin_developer_app.sh
+++ package/macports_build_plugin_developer_app.sh
@@ -29,7 +29,23 @@ cp -r ${GIMP_PREFIX}/include ${PACKAGE_DIR}/GIMP.app/Contents/Resources/
 cp -r ${GIMP_PREFIX}/lib/glib-2.0 ${PACKAGE_DIR}/GIMP.app/Contents/Resources/lib
 cp -r ${GIMP_PREFIX}/lib/pkgconfig ${PACKAGE_DIR}/GIMP.app/Contents/Resources/lib/
 
-echo "fixing pkg-config files"
-find ${PACKAGE_DIR}/GIMP.app/Contents/Resources/lib/pkgconfig -name '*.pc' -type f -exec sed -i '' -e 's@^prefix=.*$@prefix=${pcfiledir}/../..@' -e "s@${GIMP_PREFIX}@\${prefix}@g" {} \;
-find ${PACKAGE_DIR}/GIMP.app/Contents/Resources/Library/Frameworks/Python.framework/Versions/${PYTHON_VERSION}/lib/pkgconfig -name '*.pc' -type f -exec sed -i '' -e 's@^prefix=.*$@prefix=${pcfiledir}/../..@' -e "s@${GIMP_PREFIX}@\${prefix}@g" {} \;
+echo "fixing orphaned pkg-config files (graphite2, netpbm, soxr, soxr-lsr)"
+PKGCONFIG_DIR="${PACKAGE_DIR}/GIMP.app/Contents/Resources/lib/pkgconfig"
+for pc in graphite2.pc netpbm.pc soxr.pc soxr-lsr.pc; do
+  f="${PKGCONFIG_DIR}/${pc}"
+  if [ -f "$f" ]; then
+    printf "prefix=\${pcfiledir}/../..\nincludedir=\${prefix}/include\nlibdir=\${prefix}/lib\n\n" | cat - "$f" > /tmp/pc_fix_tmp && mv /tmp/pc_fix_tmp "$f"
+    sed -i '' \
+      -e "s@-L${GIMP_PREFIX}/lib@-L\${libdir}@g" \
+      -e "s@-I${GIMP_PREFIX}/include@-I\${includedir}@g" \
+      "$f"
+  fi
+done
+
+echo "fixing remaining pkg-config files"
+# shellcheck disable=SC2016
+find "${PACKAGE_DIR}/GIMP.app/Contents/Resources/lib/pkgconfig" -name '*.pc' -type f -exec sed -i '' -e 's@^prefix=.*$@prefix=${pcfiledir}/../..@' -e "s@${GIMP_PREFIX}@\${prefix}@g" {} \;
+# shellcheck disable=SC2016
+find "${PACKAGE_DIR}/GIMP.app/Contents/Resources/Library/Frameworks/Python.framework/Versions/${PYTHON_VERSION}/lib/pkgconfig" -name '*.pc' -type f -exec sed -i '' -e 's@^prefix=.*$@prefix=${pcfiledir}/../..@' -e "s@${GIMP_PREFIX}@\${prefix}@g" {} \;
+
 echo "Done developer bundling"
